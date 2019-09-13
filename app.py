43a20b46f6d98190b66a5dc980e747585f741d35
kumarjay <39044853+kumarjay@users.noreply.github.com>
2019-09-13 19:46:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, request
+from scipy.misc import imsave, imread, imresize
+import numpy as np
+import keras.models
+import re
+import base64
+import sys
+import os
+
+sys.path.append(os.path.abspath('./model'))
+from load import *
+
+global model, graph
+
+app= Flask(__name__)
+
+
+model, graph= init()
+
+print(graph.as_default())
+
+#def convertImage(imgData1):
+#    imgstr= re.search(r'base64,(.*)',imgData1).group(1)
+#    
+#    with open('output.png','wb') as output:
+#        output.write(imgstr.decode('base64'))
+
+def convertImage(imgData1): 
+    imgstr = re.search(b'base64,(.*)',imgData1).group(1) 
+    #print(imgstr) 
+    with open('output.png','wb') as output: 
+        output.write(base64.b64decode(imgstr))
+#
+@app.route('/')
+def index():
+    return render_template('index.html')
+#
+@app.route('/predict/', methods=['GET','POST'])
+def predict():
+    imgData= request.get_data()
+    convertImage(imgData)
+    print('debug 1')
+    
+    x= imread('output.png', mode='L')
+    x= np.invert(x)
+    x= imresize(x, (28,28))
+    x= x.reshape(1,28,28,1)
+    print('debug 2')
+    
+    with graph.as_default():
+        out= model.predict(x)
+        print(out)
+        print(np.argmax(out, axis=1))
+        print('debug 3')
+        response= np.array_str(np.argmax(out, axis=1))
+        return response
+    
+    
+#    
+#@app.route('/predict/',methods=['GET','POST'])
+#def predict():
+#	#whenever the predict method is called, we're going
+#	#to input the user drawn character as an image into the model
+#	#perform inference, and return the classification
+#	#get the raw data format of the image
+#	imgData = request.get_data()
+#	#encode it into a suitable format
+#	convertImage(imgData)
+#	#print "debug"
+#	#read the image into memory
+#	x = imread('output.png',mode='L')
+#	#compute a bit-wise inversion so black becomes white and vice versa
+#	x = np.invert(x)
+#	#make it the right size
+#	x = imresize(x,(28,28))
+#	#imshow(x)
+#	#convert to a 4D tensor to feed into our model
+#	x = x.reshape(1,28,28,1)
+#	#print "debug2"
+#	#in our computation graph
+#	with graph.as_default():
+#		#perform the prediction
+#		out = model.predict(x)
+#		#print(out)
+#		#print(np.argmax(out,axis=1))
+#		#print "debug3"
+#		#convert the response to a string
+#		response = np.array_str(np.argmax(out,axis=1))
+#		return response	
+	
+#    
+#if __name__=='__main__':
+#    app.run(debug=True)
+
+
+#from flask import Flask, render_template,request
+##scientific computing library for saving, reading, and resizing images
+#from scipy.misc import imsave, imread, imresize
+##for matrix math
+#import numpy as np
+##for importing our keras model
+#import keras.models
+##for regular expressions, saves time dealing with string data
+#import re
+#
+##system level operations (like loading files)
+#import sys 
+##for reading operating system data
+#import os
+##tell our app where our saved model is
+#sys.path.append(os.path.abspath("./model"))
+#from load import * 
+##initalize our flask app
+#app = Flask(__name__)
+##global vars for easy reusability
+#global model, graph
+##initialize these variables
+#model, graph = init()
+
+#decoding an image from base64 into raw representation
+#def convertImage(imgData1):
+#	imgstr = re.search(r'base64,(.*)',imgData1).group(1)
+#	#print(imgstr)
+#	with open('output.png','wb') as output:
+#		output.write(imgstr.decode('base64'))
+	
+#def convertImage(imgData1): 
+#    imgstr = re.search(b'base64,(.*)',imgData1).group(1) 
+#    #print(imgstr) 
+#    with open('output.png','wb') as output: 
+#        output.write(base64.b64decode(imgstr))
+
+#@app.route('/')
+#def index():
+#	#initModel()
+#	#render out pre-built HTML file right on the index page
+#	return render_template("index.html")
+#
+#@app.route('/predict/',methods=['GET','POST'])
+#def predict():
+#	#whenever the predict method is called, we're going
+#	#to input the user drawn character as an image into the model
+#	#perform inference, and return the classification
+#	#get the raw data format of the image
+#	imgData = request.get_data()
+#	#encode it into a suitable format
+#	convertImage(imgData)
+#	#print "debug"
+#	#read the image into memory
+#	x = imread('output.png',mode='L')
+#	#compute a bit-wise inversion so black becomes white and vice versa
+#	x = np.invert(x)
+#	#make it the right size
+#	x = imresize(x,(28,28))
+#	#imshow(x)
+#	#convert to a 4D tensor to feed into our model
+#	x = x.reshape(1,28,28,1)
+#	#print "debug2"
+#	#in our computation graph
+#	with graph.as_default():
+#		#perform the prediction
+#		out = model.predict(x)
+#		#print(out)
+#		#print(np.argmax(out,axis=1))
+#		#print "debug3"
+#		#convert the response to a string
+#		response = np.array_str(np.argmax(out,axis=1))
+#		return response	
+#	
+
+if __name__ == "__main__":
+	#decide what port to run the app in
+    #	port = int(os.environ.get('PORT', 5000))
+	#run the app locally on the givn port
+    #	app.run(host='0.0.0.0', port=port)
+	#optional if we want to run in debugging mode
+	app.run(debug=True)
