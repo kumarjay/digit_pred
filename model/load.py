05573de0030be7c6c2b9e5c50b9265bc282971e9
kumarjay <39044853+kumarjay@users.noreply.github.com>
2019-09-13 23:52:09 +0530

    Update load.py

--- model/load.py
+++ model/load.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import keras.models
-from keras.models import model_from_json
+import keras
 from scipy.misc import imread, imresize, imshow
 import tensorflow as tf
 
@@ -10,7 +9,7 @@ def init():
     json_file= open('model.json', 'r')
     loaded_model_json= json_file.read()
     json_file.close()
-    loaded_model= model_from_json(loaded_model_json)
+    loaded_model= keras.models.model_from_json(loaded_model_json)
     
     loaded_model.load_weights('model.h5')
     print('Loaded model from disk')
