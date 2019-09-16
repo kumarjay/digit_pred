b06dff7e2ed028844dca31dc320a63a24595a69a
kumarjay <39044853+kumarjay@users.noreply.github.com>
2019-09-16 10:22:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,25 @@
-from __future__ import print_function
 import numpy as np
 from flask import Flask, request, jsonify, render_template
-import os
-import base64
-#import scipy as sc
-import sys
-import re
+import pickle
 
-sys.path.append(os.path.abspath('./model'))
-#import load
-
-#global model, graph
-#model, graph= init()
 
 app= Flask(__name__)
+model=pickle.load(open('model.pkl','rb'))
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-def convertImage(imgData1): 
-    imgstr = re.search(b'base64,(.*)',imgData1).group(1) 
-    #print(imgstr) 
-    with open('output.png','wb') as output: 
-        output.write(base64.b64decode(imgstr))
-        
-#from scipy.misc import imsave, imread, imresize
-
-@app.route('/predict/', methods=['GET','POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    imgData= request.get_data()
-    convertImage(imgData)
-    print('debug 1')
     
-    x= sc.misc.imread('output.png', mode='L')
-    x= np.invert(x)
-    x= sc.misc.imresize(x, (28,28))
-    x= x.reshape(1,28,28,1)
-    print('debug 2')
+    int_features=[int(x) for x in request.form.values()]
+    final_features=[np.array(int_features)]
+    prediction=model.predict(final_features)
     
-    with graph.as_default():
-        out= model.predict(x)
-        print(out)
-        print(np.argmax(out, axis=1))
-        print('debug 3')
-        response= np.array_str(np.argmax(out, axis=1))
-        return response
+    output=round(prediction[0],2)
     
-    
+    return render_template('index.html', prediction_text='Salary should be ${}'.format(output))
+
 if __name__=='__main__':
     app.run(debug=True)
-    #port = int(os.environ.get('PORT', 5000))
-    #run the app locally on the givn port
-    #app.run(host='0.0.0.0', port=port)
